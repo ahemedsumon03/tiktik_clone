b16a18f642d5a80243a312ad53115150b935f9b7
Sumon Ahemed <ahemedsumon03@gmail.com>
2022-11-26 17:17:51 +0600

    change index.ts file

--- utils/index.ts
+++ utils/index.ts
@@ -16,6 +16,6 @@ export const createOrderUser = async (response: any, addUser:any) => {
 
   addUser(user);
 
-  await axios.post('http://localhost:3000/api/auth', user);
+  await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth`, user);
 
 }
